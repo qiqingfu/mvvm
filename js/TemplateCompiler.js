eb23c344bfd964607a3631f12e8813e890fdbba4
qiqingfu <723469354@qq.com>
2018-06-24 01:44:47 +0800

    {{}} 解析表达式

--- js/TemplateCompiler.js
+++ js/TemplateCompiler.js
@@ -6,15 +6,11 @@ class TemplateCompiler {
         this.vm = vm
 
         if (this.el) {
-            // 判断视图存在
-            // 解析指令 - 把模板放入内存,在内存中操作dom和页面没有关系,不会影响页面性能
-            // 1. fragment.appendChild(el.firstChild) 存放内存(片段),操作完之后再从内存中返回
             // 将模板内容放入内存(片段)
             let fragment = this.node2fragment(this.el)
             // 2. 解析模板
             this.compile(fragment)
             // 3. 把内存的结果返回到页面中
-            // 将解析好的模板返回到页面 this.el中
             this.el.appendChild(fragment)
         }
         
@@ -45,27 +41,36 @@ class TemplateCompiler {
         while ( child = node.firstChild ) {
             fragment.appendChild(child)
         }
-        // 3.返回
         return fragment 
     }
 
     // 解析模板
     compile (parent) {
-      // 1.获取内存中模板的子节点
       let childNodes = parent.childNodes
-      // 2. 遍历每一个节点
+      // 遍历每一个节点
       this.toArray( childNodes ).forEach((node) => {
           // 元素节点(解析指令)
           if(this.isElementNode(node)){
              this.compileElement(node)
+          }else {
+              // 定义文本表达式验证规则
+              let textReg = /\{\{(.+)\}\}/
+              let expr = node.textContent.trim()
+              if(textReg.test(expr)){
+                 let exptext = expr.replace(textReg,function($0,$1){
+                    return $1
+                 })
+                 
+                 // exptext 为 {{message}}表达式中的 message
+                 // 解析表达式
+                 this.compileText(node, exptext)
+              }
           }
 
       })
-       
       // 3. 判断节点类型
       // 1) 属性节点(解析指令)
       // 2) 文本节点(解析指令)
-
       // 4. 如果还有子节点,递归
     }
 
@@ -75,18 +80,13 @@ class TemplateCompiler {
      * @ compileText : 解析表达式 {{}}
      */
     compileElement (node) {
-
-        // 1. 获取当前元素节点的所有属性
         let arrs = node.attributes;
-        // 2. 遍历当前元素的所有属性
+        // 遍历当前元素的所有属性
         this.toArray(arrs).forEach((arr) => {
             let arrName = arr.name;
-             // 3. 判断属性是不是指令属性
+             // 判断属性是不是指令属性
             if(this.isDirective(arrName)){
-                // 4. 收集
                 let type = arrName.slice(2);
-                
-                // 符合条件指令的value
                 let expr = arr.value
 
                 // 开始数据对应到模板,动态类型
@@ -95,8 +95,8 @@ class TemplateCompiler {
         })
     }
 
-    compileText () {
-
+    compileText (node, exptext) {
+        CompilerUtils.text (node, this.vm, exptext)
     }
 }
 
@@ -117,11 +117,8 @@ CompilerUtils = {
         updateFn && updateFn(node,vm.$data[expr])
      },
 
+     
     // 定义更新规则
-    /**
-     * v-text 将数据更新到html
-     * v-model 将数据更新到value
-     */
     updater:{
         /**
          * @node 节点
